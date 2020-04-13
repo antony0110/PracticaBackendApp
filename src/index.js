96e9d2c4ddc91e776be244cfb2677eb598184f17
Antony Gomez <60438018+antony1010@users.noreply.github.com>
2020-04-13 17:17:06 +0200

    Configurando el servidor y creando rutas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-const express = require('espress');
+const express = require('express');
 const morgan = require ('morgan');
+const exhbs = require ('express-handlebars')
+const path = require('path');
+
 
 //Inicializar 
 const app = express();
 
 //settings
 app.set('port', process.env.Port || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exhbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname:'.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Global Variables 
-
+app.use((req, res, next) => {
+    next();
+})
 
 //Routes
-
+app.use(require('./routes'));
 
 //Public 
 
